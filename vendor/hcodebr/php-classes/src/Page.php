96ba9f9f5ed1439919d83480f379c8d83ed9c7be
Gabriel-analise <analise.gabriel@hotmail.com>
2024-09-17 19:26:34 -0300

    Tela de Login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -7,6 +7,8 @@ class Page {
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header" => true,
+        "footer" => true,
         "data" => []
     ];
     public function __construct($opts = array(), $tpl_dir = "/views/") {
@@ -24,7 +26,8 @@ class Page {
         
         $this->setData($this->options['data']);
 
-        $this->tpl->draw("header");
+        if($this->options["header"])
+            $this->tpl->draw("header");
     }
 
     private function setData($data = array()){
@@ -43,9 +46,8 @@ class Page {
     }
 
     public function __destruct() {
-
-        $this->tpl->draw("footer");
-        
+        if($this->options["footer"])
+            $this->tpl->draw("footer");        
     }
 }
 
